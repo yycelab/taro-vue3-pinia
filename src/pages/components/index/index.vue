0a1ad48466cc5102a63fbe3a9ad65504cca5a7f6
JohnOnion <please_enjoy_self@163.com>
2023-06-03 18:07:53 +0800

    cc

--- src/pages/components/index/index.vue
+++ src/pages/components/index/index.vue
@@ -27,13 +27,21 @@
                 </at-button>
             </at-flex-item>
             <at-flex-item class="item-goto">
+                <at-noticebar>通过中间转发页面(/pages/dispatcher/index)实现重定向</at-noticebar>
+            </at-flex-item>
+            <at-flex-item class="item-goto">
+                <at-noticebar>重定向到小程序注册页</at-noticebar>
                 <at-button @click="handleGoto({ path: indexPageUrl, extern: true })">
                     页面重定向 /pages/user/index
                 </at-button>
             </at-flex-item>
             <at-flex-item class="item-goto">
+                <at-noticebar>
+                    <view>重定向到vue路由页面(vue-router)</view>
+                    <view style="padding-left:36px;">->(/pages/index/index)->(/tab/user)</view>
+                </at-noticebar>
                 <at-button @click="handleGoto({ path: vuePageUrl, extern: true })">
-                    复杂重定向 /pages/index/index
+                    <view>复杂重定向 /tab/user</view>
                 </at-button>
             </at-flex-item>
         </at-flex>
@@ -44,11 +52,12 @@
 import './index.scss'
 import TopNav from "@/components/top-nav/index.vue"
 import { useAppStore, TabHome, TabUser } from "@/stores/index"
+import { AtNoticebar } from 'taro-ui-vue3';
 // import { onActivated, onMounted } from 'vue';
 // import {useRouter} from "@tarojs/taro"
 
-const indexPageUrl = `/pages/dispatcher/index?rto=${encodeURIComponent('/pages/user/index')}&rt=native&pks=id|name&id=1&name=${encodeURIComponent('小明')}`
-const vuePageUrl = `/pages/dispatcher/index?rto=${encodeURIComponent('/pages/index/index')}&vrto=${encodeURIComponent('/tab/user')}&pks=name|id&id=1&name=${encodeURIComponent("复杂测试")}`
+const indexPageUrl = `/pages/dispatcher/index?rto=${encodeURIComponent('/pages/user/index')}&rt=native&pks=id|name&id=1&name=${encodeURIComponent('页面重定向')}`
+const vuePageUrl = `/pages/dispatcher/index?rto=${encodeURIComponent('/pages/index/index')}&vrto=${encodeURIComponent('/tab/user')}&pks=name|id&id=2&name=${encodeURIComponent("复杂重定向")}`
 // import { useRouter } from "vue-router"
 const app = useAppStore()
 // const router = useRouter()
